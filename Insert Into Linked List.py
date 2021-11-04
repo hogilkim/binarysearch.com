92ec8552879fdb391f82a92fb4ca3c0d4bb0c627
Hogyeong Kim <hogilkim@gmail.com>
2021-11-04 17:34:15 -0400

    Nov 4 update

--- Insert Into Linked List.py
+++ Insert Into Linked List.py
@@ -2,20 +2,35 @@
 #     def __init__(self, val, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def solve(self, head, pos, val):
         if pos == 0:
-            return_node = LLNode(val, next=head)
-            return return_node
+            return LLNode(val, next=head)
 
-        insert_node_index = 1
         inserting_prev = head
 
-        while insert_node_index != pos:
+        for i in range(pos-1):
             inserting_prev = inserting_prev.next
-            insert_node_index += 1
-        inserting = LLNode(val)
-        inserting_next = inserting_prev.next
-        inserting_prev.next = inserting
-        inserting.next = inserting_next
+            
+        inserting_prev.next = LLNode(val, inserting_prev.next)
         return head
+
+        
+# class Solution:
+#     def solve(self, head, pos, val):
+#         if pos == 0:
+#             return_node = LLNode(val, next=head)
+#             return return_node
+
+#         insert_node_index = 1
+#         inserting_prev = head
+
+#         while insert_node_index != pos:
+#             inserting_prev = inserting_prev.next
+#             insert_node_index += 1
+#         inserting = LLNode(val)
+#         inserting_next = inserting_prev.next
+#         inserting_prev.next = inserting
+#         inserting.next = inserting_next
+#         return head
